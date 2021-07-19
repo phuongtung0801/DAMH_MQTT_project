e0d0b9658979c8ae53d0cf897cc58c15b205ab06
phuongtung0801 <phuongtung0801@gmail.com>
2021-07-19 09:27:57 +0700

    cd

--- light_node/src/main.cpp
+++ light_node/src/main.cpp
@@ -1,69 +1,67 @@
-//************************************************************
-// this is a simple example that uses the painlessMesh library to 
-// setup a node that logs to a central logging node
-// The logServer example shows how to configure the central logging nodes
-//************************************************************
+
 #include <painlessMesh.h>
 #include <Arduino.h>
 
-#define   MESH_PREFIX     "tung"
-#define   MESH_PASSWORD   "phuongtung0801"
-#define   MESH_PORT       5555
+#define MESH_PREFIX "tung"
+#define MESH_PASSWORD "phuongtung0801"
+#define MESH_PORT 5555
 
 #define LED D0
 #define LED1 D3
 
-Scheduler     userScheduler; // to control your personal task
-painlessMesh  mesh;
+Scheduler userScheduler; // to control your personal task
+painlessMesh mesh;
 
 // Prototype
-void receivedCallback( uint32_t from, String &msg );
+void receivedCallback(uint32_t from, String &msg);
 
 size_t logServerId = 0;
 
-// Send message to the logServer every 10 seconds 
-Task myLoggingTask(3000, TASK_FOREVER, []() {
-#if ARDUINOJSON_VERSION_MAJOR==6
-        DynamicJsonDocument jsonBuffer(1024);
-        JsonObject msg = jsonBuffer.to<JsonObject>();
+// Send message to the logServer every 10 seconds
+Task myLoggingTask(3000, TASK_FOREVER, []()
+                   {
+#if ARDUINOJSON_VERSION_MAJOR == 6
+                     DynamicJsonDocument jsonBuffer(1024);
+                     JsonObject msg = jsonBuffer.to<JsonObject>();
 #else
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& msg = jsonBuffer.createObject();
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject &msg = jsonBuffer.createObject();
 #endif
-    msg["nodename"] = "light_sensor";
-    msg["nodeID"] = mesh.getNodeId();
-    msg["topic"] = "light";
-    msg["light"] = String(analogRead(A0));    
+                     msg["nodename"] = "light_sensor";
+                     msg["nodeID"] = mesh.getNodeId();
+                     msg["topic"] = "light";
+                     msg["light"] = String(analogRead(A0));
 
-    String str;
-#if ARDUINOJSON_VERSION_MAJOR==6
-    serializeJson(msg, str);
+                     String str;
+#if ARDUINOJSON_VERSION_MAJOR == 6
+                     serializeJson(msg, str);
 #else
-    msg.printTo(str);
+      msg.printTo(str);
 #endif
-    if (logServerId == 0) // If we don't know the logServer yet
-        mesh.sendBroadcast(str);
-    else
-        mesh.sendSingle(logServerId, str);
+                     if (logServerId == 0) // If we don't know the logServer yet
+                       mesh.sendBroadcast(str);
+                     else
+                       mesh.sendSingle(logServerId, str);
 
     // log to serial
-#if ARDUINOJSON_VERSION_MAJOR==6
-    serializeJson(msg, Serial);
+#if ARDUINOJSON_VERSION_MAJOR == 6
+                     serializeJson(msg, Serial);
 #else
-    msg.printTo(Serial);
+      msg.printTo(Serial);
 #endif
-    Serial.printf("\n");
-});
+                     Serial.printf("\n");
+                   });
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(A0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED, OUTPUT);
-    
-  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 6 );
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
+
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 6);
   mesh.onReceive(&receivedCallback);
 
   // Add the task to the your scheduler
@@ -71,47 +69,60 @@ void setup() {
   myLoggingTask.enable();
 }
 
-void loop() {
-    userScheduler.execute(); // it will run mesh scheduler as well
-    mesh.update();
+void loop()
+{
+  userScheduler.execute(); // it will run mesh scheduler as well
+  mesh.update();
 }
 
-void receivedCallback( uint32_t from, String &msg ) {
+void receivedCallback(uint32_t from, String &msg)
+{
   Serial.printf("logClient: Received from %u msg=%s\n", from, msg.c_str());
-    //String cmd = String(msg);
-  if (msg == "off") { digitalWrite(LED, HIGH); }   // LED off
-  if (msg == "on") { digitalWrite(LED, LOW); } // LED on
+  //String cmd = String(msg);
+  if (msg == "off")
+  {
+    digitalWrite(LED, HIGH);
+  } // LED off
+  if (msg == "on")
+  {
+    digitalWrite(LED, LOW);
+  } // LED on
   // Saving logServer
-#if ARDUINOJSON_VERSION_MAJOR==6
+#if ARDUINOJSON_VERSION_MAJOR == 6
   DynamicJsonDocument jsonBuffer(1024 + msg.length());
   DeserializationError error = deserializeJson(jsonBuffer, msg);
-  if (error) {
+  if (error)
+  {
     Serial.printf("DeserializationError\n");
     return;
   }
   JsonObject root = jsonBuffer.as<JsonObject>();
 #else
   DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(msg);
+  JsonObject &root = jsonBuffer.parseObject(msg);
 #endif
-  if (root.containsKey("topic")) {
-      if (String("logServer").equals(root["topic"].as<String>())) {
-          // check for on: true or false
-          logServerId = root["nodeId"];
-          Serial.printf("logServer detected!!!\n");
+  if (root.containsKey("topic"))
+  {
+    if (String("logServer").equals(root["topic"].as<String>()))
+    {
+      // check for on: true or false
+      logServerId = root["nodeId"];
+      Serial.printf("logServer detected!!!\n");
+    }
+    else if (String("exeNode").equals(root["topic"].as<String>()))
+    {
+      String exeCmd = root["exeCmd"].as<String>();
+      if (exeCmd == "1")
+      {
+        digitalWrite(4, HIGH);
+        Serial.printf("Turn on LED !");
       }
-      else if (String("exeNode").equals(root["topic"].as<String>())){
-        String exeCmd = root["exeCmd"].as<String>();
-        if (exeCmd == "1")
-        {
-          digitalWrite(4, HIGH);
-          Serial.printf("Turn on LED !");
-        } else
-        {
-          digitalWrite(4, LOW);
-          Serial.printf("Turn off LED !");
-        }
-     }
-     Serial.printf("Handled from %u msg=%s\n", from, msg.c_str());
-  }  
+      else
+      {
+        digitalWrite(4, LOW);
+        Serial.printf("Turn off LED !");
+      }
+    }
+    Serial.printf("Handled from %u msg=%s\n", from, msg.c_str());
+  }
 }
--- server_node/src/main.cpp
+++ server_node/src/main.cpp
@@ -1,12 +1,4 @@
-//************************************************************
-// this is a simple example that uses the painlessMesh library to
-// connect to a another network and relay messages from a MQTT broker to the nodes of the mesh network.
-// To send a message to a mesh node, you can publish it to "painlessMesh/to/12345678" where 12345678 equals the nodeId.
-// To broadcast a message to all nodes in the mesh you can publish it to "painlessMesh/to/broadcast".
-// When you publish "getNodes" to "painlessMesh/to/gateway" you receive the mesh topology as JSON
-// Every message from the mesh which is send to the gateway node will be published to "painlessMesh/from/12345678" where 12345678 
-// is the nodeId from which the packet was send.
-//************************************************************
+
 
 #include <Arduino.h>
 #include <painlessMesh.h>
@@ -14,13 +6,12 @@
 #include <FirebaseESP8266.h>
 #include <WiFiClient.h>
 
+#define MESH_PREFIX "tung"
+#define MESH_PASSWORD "phuongtung0801"
+#define MESH_PORT 5555
 
-#define   MESH_PREFIX     "tung"
-#define   MESH_PASSWORD   "phuongtung0801"
-#define   MESH_PORT       5555
-
-#define   STATION_SSID     "TUNG"
-#define   STATION_PASSWORD "123456789"
+#define STATION_SSID "TUNG"
+#define STATION_PASSWORD "123456789"
 //WiFiEventHandler wifiConnectHandler;
 //WiFiEventHandler wifiDisconnectHandler;
 
@@ -35,113 +26,66 @@ unsigned long t1 = 0;
 const unsigned long eventInterval = 5000;
 unsigned long previousTime = 0;
 
-
-const char* mqtt_server = "m14.cloudmqtt.com";
-const char* mqtt_username = "bxpalvco";
-const char* mqtt_password = "UUILhS73phGV";
-const char* clientID = "TUNG";
-const char* endTopic = "phuongtung0801/LWT";
+const char *mqtt_server = "m14.cloudmqtt.com";
+const char *mqtt_username = "bxpalvco";
+const char *mqtt_password = "UUILhS73phGV";
+const char *clientID = "TUNG";
+const char *endTopic = "phuongtung0801/LWT";
 
 // Prototypes
-void receivedCallback( const uint32_t &from, const String &msg );
-void mqttCallback(char* topic, byte* payload, unsigned int length);
-
-/*void initWifi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(STATION_SSID, STATION_PASSWORD);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.print("Connecting to: ");
-  Serial.println(WiFi.localIP());
-} */
-
-/*void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi sucessfully.");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi, trying to connect...");
-  WiFi.disconnect();
-  WiFi.begin(STATION_SSID, STATION_PASSWORD);
-}*/
-
-
+void receivedCallback(const uint32_t &from, const String &msg);
+void mqttCallback(char *topic, byte *payload, unsigned int length);
 
 IPAddress getlocalIP();
 
-IPAddress myIP(0,0,0,0);
+IPAddress myIP(0, 0, 0, 0);
 //IPAddress mqttBroker("m2m.eclipse.org");
 
-painlessMesh  mesh;
+painlessMesh mesh;
 WiFiClient wifiClient;
 //tạo một instance để kết nối. Sử dụng cú pháp PubSubClient (server, port, [callback], client, [stream])
 
 //PubSubClient mqttClient(wifiClient);
 PubSubClient mqttClient(mqtt_server, 12321, mqttCallback, wifiClient);
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-  /*wifi checking
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);*/
-  /*Serial.print("RRSI: ");
-  Serial.println(WiFi.RSSI());*/
-   
-
-  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
 
   // Channel set to 6. Make sure to use the same channel for your mesh and for you other
   // network (STATION_SSID)
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6);
   mesh.onReceive(&receivedCallback);
 
   mesh.stationManual(STATION_SSID, STATION_PASSWORD);
   mesh.setHostname(HOSTNAME);
 
   WiFi.begin(STATION_SSID, STATION_PASSWORD);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
   WiFi.setAutoReconnect(true);
   WiFi.persistent(true);
-//kết nối mqtt broker
+  //kết nối mqtt broker
   mqttClient.setServer(mqtt_server, 12321);
   mqttClient.setCallback(mqttCallback);
   //mqttClient.connect(clientID, "", "", "theEndTopic", 1, true, "offline");
   if (mqttClient.connect(
-    clientID, mqtt_username, mqtt_password, endTopic, 1, true, "Sensor disconnected from mqtt"))
+          clientID, mqtt_username, mqtt_password, endTopic, 1, true, "Sensor disconnected from mqtt"))
   {
     Serial.println("Connected to MQTT Broker!");
     mqttClient.publish(endTopic, "Sensor connected!", true);
   }
-  else {
+  else
+  {
     Serial.println("Connection to MQTT Broker failed...");
   }
-
-  /*while (!mqttClient.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (mqttClient.connect(
-      clientID, mqtt_username, mqtt_password, endTopic, 1, true, "Sensor disconnected from mqtt" ))
-    {
-      Serial.println("connected");  
-    } 
-    else 
-    { 
-      Serial.print("failed with state ");
-      Serial.print(mqttClient.state());
-      delay(2000); 
-    }
-  }*/
 
   mqttClient.subscribe("tungtran");
   mqttClient.subscribe(endTopic);
@@ -156,67 +100,58 @@ void setup() {
   }
 }
 
-void loop() {
+void loop()
+{
   mesh.update();
   mqttClient.loop();
-  /*firebase test
-  if (millis() - t1 > 1000)
-  { Firebase.setInt(firebaseData, path + "/data", random(0,100));
-    t1 = millis();
-  }*/
 
   /* Updates frequently */
   unsigned long currentTime = millis();
 
   //wifi checking
-  if(currentTime - previousTime >= eventInterval)
+  if (currentTime - previousTime >= eventInterval)
   {
     switch (WiFi.status())
     {
     case WL_CONNECTED:
       //Serial.println("Wifi Connection successfully established");
       if (!mqttClient.connected())
-    {
-      Serial.println("Reconnecting to MQTT...");
-      if (mqttClient.connect(
-        clientID, mqtt_username, mqtt_password, endTopic, 1, true, "Sensor disconnected from mqtt"))
-    {
-      Serial.println("Reconnect to MQTT success!");  
-      mqttClient.setServer(mqtt_server, 12321);
-      mqttClient.setCallback(mqttCallback);
-      mqttClient.subscribe("tungtran");
-      mqttClient.publish(endTopic, "Sensor connected!", true);
-    } 
-    else 
-    { 
-      Serial.println("MQTT connection is lost!");
-    }
-    }
+      {
+        Serial.println("Reconnecting to MQTT...");
+        if (mqttClient.connect(
+                clientID, mqtt_username, mqtt_password, endTopic, 1, true, "Sensor disconnected from mqtt"))
+        {
+          Serial.println("Reconnect to MQTT success!");
+          mqttClient.setServer(mqtt_server, 12321);
+          mqttClient.setCallback(mqttCallback);
+          mqttClient.subscribe("tungtran");
+          mqttClient.publish(endTopic, "Sensor connected!", true);
+        }
+        else
+        {
+          Serial.println("MQTT connection is lost!");
+        }
+      }
       break;
-    
-    default: Serial.println("Failed to connect Wifi");
+
+    default:
+      Serial.println("Failed to connect Wifi");
       break;
     }
     //Update timing for next round
     previousTime = currentTime;
   }
-  
 
-  if(myIP != getlocalIP()){
+  if (myIP != getlocalIP())
+  {
     myIP = getlocalIP();
     Serial.println("My IP is " + myIP.toString());
-
-    /*if (mqttClient.connect("painlessMeshClient"))
-    {
-      mqttClient.publish("painlessMesh/from/gateway", "Ready!");
-      mqttClient.subscribe("tungtran");
-    }*/
-    //mqttClient.subscribe("tungtran");
   }
 }
 
-void receivedCallback( const uint32_t &from, const String &msg ) {
-  Serial.printf("bridge: Received from %u msg=%s\n",  from, msg.c_str());
+void receivedCallback(const uint32_t &from, const String &msg)
+{
+  Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
   //String topic = "painlessMesh/from/2131961461";
   String topic = "painlessMesh/from/" + String(from);
   mqttClient.publish(topic.c_str(), msg.c_str());
@@ -225,16 +160,18 @@ void receivedCallback( const uint32_t &from, const String &msg ) {
   Serial.println("Data received");
 }
 
-void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
+void mqttCallback(char *topic, uint8_t *payload, unsigned int length)
+{
 
-  String cmd ="";
-  
+  String cmd = "";
+
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     cmd += (char)payload[i];
-  }  
+  }
   Serial.println(cmd);
   if (cmd == "on")
   {
@@ -247,7 +184,7 @@ void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
 }
 //end mqttCallback
 
-IPAddress getlocalIP() {
+IPAddress getlocalIP()
+{
   return IPAddress(mesh.getStationIP());
 }
-
